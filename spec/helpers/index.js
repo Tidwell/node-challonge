b38dd0730a919313791ddf05ab093939c44d3c24
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2017-03-14 21:42:54 -0400

    add request callback tests

--- spec/helpers/index.js
+++ spec/helpers/index.js
@@ -5,17 +5,23 @@ mockery.enable({
 });
 
 const httpsMock = {
+	reset: function() {
+		this.listeners = {};
+		this.opts = {};
+		this.res.statusCode = '';
+	},
 	opts: {},
 	listeners: {
 	},
+	res: {
+		on: (method, cb) => {
+			if (!httpsMock.listeners[method]) { httpsMock.listeners[method] = []; }
+			httpsMock.listeners[method].push(cb);
+		}
+	},
 	request: function(opts, pipe) {
 		this.opts = opts;
-		pipe({
-			on: (method, cb) => {
-				if (!this.listeners[method]) { this.listeners[method] = []; }
-				this.listeners[method].push(cb);
-			}
-		});
+		pipe(this.res);
 
 		return {
 			end: () => {}
